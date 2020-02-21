f598f616c64fbb737f84e3b48be0e653d5cb84f7
Heith Robbins <heith3214@gmail.com>
2020-02-21 10:47:54 -0700

    Maping data

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import PortfolioItem from "./portfolioitem"
+import portfolioitem from './portfolioitem';
+
 
 
 export default class PortfolioContainer extends Component {
@@ -8,11 +10,22 @@ export default class PortfolioContainer extends Component {
         super()
         console.log("Portfolio container have rendered")
     }
+
+    portfolioItems() {
+        const data = ["Quip", "Eventbrite", "Ministry Safe"]
+
+        return data.map(item => {
+            return <PortfolioItem />
+        }) 
+        
+    }
+
     render() {
         return (
             <div>
                 <h2>portfolio items go here</h2>
-                <PortfolioItem />
+
+                {this.portfolioItems()}
             </div>
         )
     }

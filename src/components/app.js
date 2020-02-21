d3f3fe790eb842395b1a81dd1c220ab194a3c674
Heith Robbins <heith3214@gmail.com>
2020-02-21 10:09:11 -0700

    created portfolio folder

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
-import PortfolioContainer from "./portfolio-container";
+import PortfolioContainer from "./portfolio/portfolio-container";
 
 export default class App extends Component {
   render() {
     return (
-      <div className="app">
+      <div className="app"> 
         <h1>Heith Robbins Portfolio</h1>
-        <PortfolioContainer />
-        <PortfolioContainer />
-        <PortfolioContainer />
         <div>{moment().format("MMMM Do YY, h:mm:ss a")}</div>
+        <PortfolioContainer />
       </div>
     );
   }
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import PortfolioItem from "./portfolioitem"
+import portfolioitem from './portfolioitem';
+
 export default class PortfolioContainer extends Component {
     render() {
         return (
             <div>
                 <h2>portfolio items go here</h2>
+                <PortfolioItem />
             </div>
         )
     }

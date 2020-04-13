0471c5c6514bb89c9fdc9c8bf681850d94f551e2
Heith Robbins <heith3214@gmail.com>
2020-04-13 15:46:11 -0600

    added more styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import moment from 'moment';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import NavigationContainer from "./navigation/navigation-container"
@@ -16,11 +15,10 @@ export default class App extends Component {
   render() {
     
     return (
-      <div className="app">
+      <div className="container">
         <Router>
           <div>
-            <h1>Heith Robbins Portfolio</h1>
-            <div>{moment().format("MMMM Do YY, h:mm:ss a")}</div>
+            
             <NavigationContainer />
             <Switch>
               <Route exact path="/" component={Home} />

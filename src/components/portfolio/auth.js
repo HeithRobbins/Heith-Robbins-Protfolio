81a7203c45c5ec8195afef05cf5aa78905bc072a
Heith Robbins <heith3214@gmail.com>
2020-04-21 15:39:19 -0600

    added a delete

--- src/components/portfolio/auth.js
+++ src/components/portfolio/auth.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
-import loginImg from "../../../static/assets/images/auth/login.jpg"
+import Login from "../auth/login";
+import loginImg from "../../../static/assets/images/auth/login.jpg";
 
 
 export default class Auth extends Component {
+    constructor(props) {
+        super(props)
+
+        this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+        this.handleUnsuccessfulAuth = this.handleUnsuccessfulAuth.bind(this);
+    }
+
+    handleSuccessfulAuth() {
+        this.props.handleSuccessfulLogin()
+        this.props.history.push("/")
+    }
+
+    handleUnsuccessfulAuth() {
+        this.props.handleUnsuccessfulLogin()
+    }
+
     render() {
         return (
         <div className="auth-page-wrapper">
@@ -14,6 +31,10 @@ export default class Auth extends Component {
             />
 
             <div className="right-column">
+            <Login 
+                handleSuccessfulAuth={this.handleSuccessfulAuth}
+                handleUnsuccessfulAuth={this.handleUnsuccessfulAuth}
+            />
                 <h1>Login Component goes here...</h1>
             </div>
         </div>
